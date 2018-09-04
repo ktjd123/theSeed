0210f8ca7c8929dc64dd2e6a14c286ddb540226a
ktjd1262 <k9326239@gmail.com>
2018-09-04 22:57:48 +0900

    third page done

--- frontend/src/components/Third/index.js
+++ frontend/src/components/Third/index.js
@@ -12,5 +12,37 @@ export default () => (
     <Title>
 개발환경
     </Title>
+    <div className={cx('platforms')}>
+      <h1 className={cx('p-title')}>
+Backend
+      </h1>
+      <div className={cx('wrapper')}>
+        <img src="/images/node.png" alt="node" className={cx('img')} />
+        <span className={cx('big')}>
++
+        </span>
+        <img src="/images/express.png" alt="node" className={cx('img')} />
+      </div>
+
+      <h1 className={cx('p-title')}>
+Frontend
+      </h1>
+
+      <div className={cx('wrapper')}>
+        <img src="/images/webpack.png" alt="node" className={cx('img')} />
+        <span className={cx('big')}>
++
+        </span>
+        <img src="/images/react.png" alt="node" className={cx('img')} />
+      </div>
+
+      <h1 className={cx('p-title')}>
+DB
+      </h1>
+
+      <div className={cx('wrapper')}>
+        <img src="/images/mongo.png" alt="node" className={cx('img')} />
+      </div>
+    </div>
   </div>
 );
