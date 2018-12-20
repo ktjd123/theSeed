734d04a3e995d5f405407c6db0b9fba53527dabb
ktjd1262 <k9326239@gmail.com>
2018-12-20 16:06:29 +0900

    backend, frontend done

--- server/index.js
+++ server/index.js
@@ -1,56 +1,64 @@
-const express = require('express');
+// const express = require('express');
+import express from 'express';
+
 const server = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const glob = require('glob');
 
-const next = require('next')
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const defaultRequestHandler = app.getRequestHandler()
+const next = require('next');
 
-const LOCAL_DB = 'nextjs-express-boilerplate'
-const MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost/${LOCAL_DB}`
-const PORT = process.env.PORT || 3001
+const dev = process.env.NODE_ENV === 'development';
+const app = next({ dev });
+const defaultRequestHandler = app.getRequestHandler();
+
+const LOCAL_DB = 'nextjs-express-boilerplate';
+const MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost/${LOCAL_DB}`;
+const PORT = process.env.PORT || 3000;
 
 app.prepare().then(() => {
+  // Parse application/x-www-form-urlencoded
+  server.use(bodyParser.urlencoded({ extended: false }));
+  // Parse application/json
+  server.use(bodyParser.json());
 
-	// Parse application/x-www-form-urlencoded
-	server.use(bodyParser.urlencoded({ extended: false }));
-	// Parse application/json
-	server.use(bodyParser.json());
+  // Allows for cross origin domain request:
+  server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 
-	// Allows for cross origin domain request:
-	server.use(function(req, res, next) {
-		res.header('Access-Control-Allow-Origin', '*');
-		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-		next();
-	});
+  // MongoDB
+  mongoose.Promise = Promise;
+  mongoose.connect(
+    MONGODB_URI,
+    { useMongoClient: true },
+  );
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
 
-	// MongoDB
-	mongoose.Promise = Promise;
-	mongoose.connect(MONGODB_URI, { useMongoClient: true });
-	const db = mongoose.connection;
-	db.on('error', console.error.bind(console, 'connection error:'));
+  // API routes
+  const rootPath = require('path').normalize(`${__dirname}/..`);
+  glob
+    .sync(`${rootPath}/server/routes/*.js`)
+    .forEach(controllerPath => require(controllerPath)(server));
 
-	// API routes
-	const rootPath = require('path').normalize(__dirname + '/..');
-	glob.sync(rootPath + '/server/routes/*.js').forEach(controllerPath => require(controllerPath)(server));
+  // Next.js request handling
+  const customRequestHandler = (page, req, res) => {
+    // Both query and params will be available in getInitialProps({query})
+    const mergedQuery = Object.assign({}, req.query, req.params);
+    app.render(req, res, page, mergedQuery);
+  };
 
-	// Next.js request handling
-	const customRequestHandler = (page, req, res) => {
-		// Both query and params will be available in getInitialProps({query})
-		const mergedQuery = Object.assign({}, req.query, req.params);
-		app.render(req, res, page, mergedQuery);
-	};
+  // Routes
+  // server.get('/custom', customRequestHandler.bind(undefined, '/custom-page'));
+  server.get('/', customRequestHandler.bind(undefined, '/'));
+  server.get('*', defaultRequestHandler);
 
-	// Routes
-	//server.get('/custom', customRequestHandler.bind(undefined, '/custom-page'));
-	server.get('/', customRequestHandler.bind(undefined, '/'));
-	server.get('*', defaultRequestHandler);
-
-	server.listen(PORT, function () {
-		console.log(`App running on http://localhost:${PORT}/\nAPI running on http://localhost:${PORT}/api/kittens`)
-	});
-
-})
+  server.listen(PORT, () => {
+    console.log(
+      `App running on http://localhost:${PORT}/\nAPI running on http://localhost:${PORT}/api/kittens`,
+    );
+  });
+});
--- server/routes/kittens.js
+++ server/routes/kittens.js
@@ -1,26 +1,5 @@
-'use strict';
+"use strict";
 
-const mongooseCrudify = require('mongoose-crudify');
+const Kitten = require("../models/kitten");
 
-const helpers = require('../services/helpers');
-const Kitten = require('../models/kitten');
-
-module.exports = function (server) {
-
-	// Docs: https://github.com/ryo718/mongoose-crudify
-	server.use(
-		'/api/kittens',
-		mongooseCrudify({
-			Model: Kitten,
-			selectFields: '-__v', // Hide '__v' property
-			endResponseInAction: false,
-
-			// beforeActions: [],
-			// actions: {}, // list (GET), create (POST), read (GET), update (PUT), delete (DELETE)
-			afterActions: [
-				{ middlewares: [helpers.formatResponse] },
-			],
-		})
-	);
-
-};
+module.exports = function(server) {};
