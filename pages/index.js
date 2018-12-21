da61f4cb29d1ecbf5ece30d7c78df8e30e281c11
ktjd1262 <k9326239@gmail.com>
2018-12-21 16:46:33 +0900

    link example

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Link from 'next/link';
 import Head from '../components/PageHead';
 import Default from '../components/Default';
 import Default2 from '../components/Default2';
@@ -12,6 +13,11 @@ class index extends Component {
         <Head title="index page" description="this is index page" />
         <Default />
         <Default2 />
+        <Link prefetch href="/about/peter">
+          Peter에 대하여
+        </Link>
+        <Link href="/about">About</Link>
+
         {JSON.stringify(this.props)}
       </div>
     );
