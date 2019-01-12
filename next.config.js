3b8effbf370224397566225825f9d73d65113565
ktjd1262 <k9326239@gmail.com>
2019-01-13 02:20:44 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,14 +1,22 @@
 /* eslint-disable */
 const withSass = require('@zeit/next-sass');
+const withCss = require('@zeit/next-css');
+const withPlugins = require('next-compose-plugins');
 
-module.exports = withSass({
-  cssModules: true,
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: '[folder]__[name]__[local]___[hash:base64:5]',
-  },
-  sassLoaderOptions: {
-    includePaths: ['styles'],
-    data: "@import 'utils';",
-  },
-});
+module.exports = withPlugins([
+  [withCss],
+  [
+    withSass,
+    {
+      cssModules: true,
+      cssLoaderOptions: {
+        importLoaders: 1,
+        localIdentName: '[folder]__[name]__[local]___[hash:base64:5]',
+      },
+      sassLoaderOptions: {
+        includePaths: ['styles'],
+        data: "@import 'utils';",
+      },
+    },
+  ],
+]);
