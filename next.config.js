54f4845b76e32d18a2d2c94b4ab677861663a050
ktjd1262 <k9326239@gmail.com>
2019-07-14 22:32:34 +0900

    with typescript udpate

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const withSass = require("@zeit/next-sass");
 const withCss = require("@zeit/next-css");
 const withPlugins = require("next-compose-plugins");
+const withTypescript = require("@zeit/next-typescript");
 
 module.exports = withPlugins([
   [withCss],
@@ -18,5 +19,6 @@ module.exports = withPlugins([
         data: "@import 'utils';"
       }
     }
-  ]
+  ],
+  [withTypescript]
 ]);
