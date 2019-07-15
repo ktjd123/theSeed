3d7c7ba99dc2205e5c6a8b2f9f5d97a08757daf0
ktjd1262 <k9326239@gmail.com>
2019-07-16 01:58:16 +0900

    update for nextjs

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const withSass = require("@zeit/next-sass");
 const withCss = require("@zeit/next-css");
 const withPlugins = require("next-compose-plugins");
-const withTypescript = require("@zeit/next-typescript");
 
 module.exports = withPlugins([
   [withCss],
@@ -19,6 +18,5 @@ module.exports = withPlugins([
         data: "@import 'utils';"
       }
     }
-  ],
-  [withTypescript]
+  ]
 ]);
