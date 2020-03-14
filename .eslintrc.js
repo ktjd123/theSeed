0f89c74d7aeb9daf3b144081f9164ad171efe68e
ktjd1262 <k9326239@gmail.com>
2020-03-14 16:55:53 +0900

    update linter options

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,36 @@
 module.exports = {
-	parser: "@typescript-eslint/parser",
-	plugins: ["@typescript-eslint"],
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
 	extends: [
-		"prettier",
-		"airbnb",
-		"airbnb/hooks",
-		"prettier/react",
-		"plugin:@typescript-eslint/recommended",
-		"prettier/@typescript-eslint",
-		"plugin:prettier/recommended"
+		'prettier',
+		'airbnb',
+		'airbnb/hooks',
+		'prettier/react',
+		'plugin:@typescript-eslint/recommended',
+		'prettier/@typescript-eslint',
+		'plugin:prettier/recommended'
 	],
 	rules: {
-		"react/jsx-filename-extension": [1, { extensions: [".tsx"] }],
-		"no-underscore-dangle": "off",
-		"@typescript-eslint/explicit-function-return-type": "off",
-		"@typescript-eslint/no-empty-interface": "off",
-		"import/extensions": [
-			"error",
-			"ignorePackages",
+		'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+		'no-underscore-dangle': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/no-empty-interface': 'off',
+		'jsx-a11y/anchor-is-valid': 'off',
+		'import/extensions': [
+			'error',
+			'ignorePackages',
 			{
-				js: "never",
-				jsx: "never",
-				ts: "never",
-				tsx: "never"
+				js: 'never',
+				jsx: 'never',
+				ts: 'never',
+				tsx: 'never'
 			}
 		]
 	},
 	settings: {
-		"import/resolver": {
+		'import/resolver': {
 			node: {
-				extensions: [".js", ".jsx", ".ts", ".tsx"]
+				extensions: ['.js', '.jsx', '.ts', '.tsx']
 			}
 		}
 	}
