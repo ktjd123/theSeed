11886ba7494a27f7b80aafe0bd9c1eb098804dc9
ktjd1262 <k9326239@gmail.com>
2020-07-26 13:03:49 +0900

    minor update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,20 +16,20 @@ import '../styles/default.scss';
 const auth = new AuthStore();
 
 export default class MyApp extends App {
-	render() {
-		const { Component, pageProps } = this.props;
-		return (
-			<Provider auth={auth}>
-				<PageHead />
-				<ToastContainer
-					autoClose={3000}
-					position="bottom-center"
-					toastClassName={css({
-						background: 'black',
-					})}
-				/>
-				<Component {...this.state} {...pageProps} />
-			</Provider>
-		);
-	}
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Provider auth={auth}>
+        <PageHead />
+        <ToastContainer
+          autoClose={3000}
+          position="bottom-center"
+          toastClassName={css({
+            background: 'black',
+          })}
+        />
+        <Component {...this.state} {...pageProps} />
+      </Provider>
+    );
+  }
 }
