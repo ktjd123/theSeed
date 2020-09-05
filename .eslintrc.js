38e9905bd3768392fedc01fa0793216d753e647d
ktjd1262 <k9326239@gmail.com>
2020-09-05 14:38:56 +0900

    update to prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,44 @@
 module.exports = {
-	parser: '@typescript-eslint/parser',
-	env: {
-		browser: true,
-		node: true,
-	},
-	parserOptions: {
-		tsconfigRootDir: './tsconfig.json',
-	},
-	plugins: ['@typescript-eslint'],
-	extends: [
-		'prettier',
-		'airbnb',
-		'airbnb/hooks',
-		'prettier/react',
-		'plugin:@typescript-eslint/recommended',
-		'prettier/@typescript-eslint',
-		'plugin:prettier/recommended',
-	],
-	rules: {
-		'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
-		'no-underscore-dangle': 'off',
-		'@typescript-eslint/explicit-function-return-type': 'off',
-		'@typescript-eslint/no-empty-interface': 'off',
-		'jsx-a11y/anchor-is-valid': 'off',
-		'import/extensions': [
-			'error',
-			'ignorePackages',
-			{
-				js: 'never',
-				jsx: 'never',
-				ts: 'never',
-				tsx: 'never',
-			},
-		],
-	},
-	settings: {
-		'import/resolver': {
-			node: {
-				extensions: ['.js', '.jsx', '.ts', '.tsx'],
-			},
-		},
-	},
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    tsconfigRootDir: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'prettier',
+    'airbnb',
+    'airbnb/hooks',
+    'prettier/react',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+    'no-underscore-dangle': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
 };
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
-const withSass = require("@zeit/next-sass");
-const withCss = require("@zeit/next-css");
-const withPlugins = require("next-compose-plugins");
+const withSass = require('@zeit/next-sass');
+const withCss = require('@zeit/next-css');
+const withPlugins = require('next-compose-plugins');
 
 module.exports = withPlugins([
   [withCss],
@@ -11,12 +11,12 @@ module.exports = withPlugins([
       cssModules: true,
       cssLoaderOptions: {
         importLoaders: 1,
-        localIdentName: "[folder]__[name]__[local]___[hash:base64:5]"
+        localIdentName: '[folder]__[name]__[local]___[hash:base64:5]',
       },
       sassLoaderOptions: {
-        includePaths: ["styles"],
-        data: "@import 'utils';"
-      }
-    }
-  ]
+        includePaths: ['styles'],
+        data: "@import 'utils';",
+      },
+    },
+  ],
 ]);
