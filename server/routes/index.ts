8aefa9d5c84463395f8fab771e2d041bb4e010e2
ktjd123 <k9326239@gmail.com>
2022-02-04 05:33:47 +0900

    save

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,15 +1,12 @@
-import express from "express";
-import auth from "./auth";
+import express from 'express';
+import auth from './auth';
 
 const router = express();
 
-router.use("/auth", auth);
+router.use('/auth', auth);
 
-router.get(
-  "/heart_beat",
-  async (req: express.Request, res: express.Response) => {
-    res.send();
-  }
-);
+router.get('/heart_beat', async (req: express.Request, res: express.Response) => {
+  res.send();
+});
 
 export default router;
