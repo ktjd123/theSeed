25a9977ef592afdd4c6cad692209f607006d8e75
ktjd123 <k9326239@gmail.com>
2022-02-05 01:38:17 +0900

    add code minification

--- server/index.ts
+++ server/index.ts
@@ -1,40 +1,41 @@
-import express from 'express';
-import morgan from 'morgan';
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 // import session from "express-session";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 // import MongoStore from "connect-mongo";
-import next from 'next';
+import next from "next";
 
-import api from './routes';
+import api from "./routes";
 
 const server = express();
 
-const dev = process.env.NODE_ENV === 'development';
+const dev = process.env.NODE_ENV === "development";
 const app = next({ dev });
-const handler = app.getRequestHandler();
-
-const PORT = process.env.NODE_ENV === 'development' ? 80 : 3000;
-
 app.prepare().then(() => {
-  server.use(express.urlencoded({ extended: true, limit: '10mb' }));
-  server.use(express.json({ limit: '10mb' }));
+  server.use(express.urlencoded({ extended: true, limit: "10mb" }));
+  server.use(express.json({ limit: "10mb" }));
 
   if (dev) {
-    server.use(morgan('dev'));
+    server.use(morgan("dev"));
+  } else {
+    const whiteList = ["https://dekina.com"];
+    cors({ origin: whiteList });
   }
 
   mongoose.Promise = global.Promise;
 
-  server.use('/api', api);
+  server.use("/api", api);
 
-  server.get('*', (req, res) => {
+  server.get("*", (req, res) => {
+    const handler = app.getRequestHandler();
     handler(req, res);
   });
 
   // eslint-disable-next-line no-unused-vars
   server.use((err: any, req: any, res: any, _: any) => {
     const errMsg = String(err.message);
-    if (errMsg && errMsg.includes('CORS')) {
+    if (errMsg && errMsg.includes("CORS")) {
       // console.error(`CORS ERROR ${req.origin}`);
     } else {
       console.error(err.stack);
@@ -42,6 +43,7 @@ app.prepare().then(() => {
     return res.status(500).json({ code: 0 });
   });
 
+  const PORT = dev ? 80 : 3000;
   server.listen(PORT, () => {
     console.log(`App running on http://localhost:${PORT}`);
   });
