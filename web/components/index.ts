c22ad115ea06a36f41581b92e61eeee8b6925a7f
ktjd123 <k9326239@gmail.com>
2022-02-24 17:31:39 +0900

    remove web

--- web/components/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as PageHead } from './PageHead';
--- web/server/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express();
-
-router.get("/heart_beat", async (req, res) => {
-  res.sendStatus(200);
-});
-
-export default router;
